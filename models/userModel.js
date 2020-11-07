126b5eeb3a309be0793cd57dbb9ba7eb4b310b9e
vertefra <verte.fra@gmail.com>
2020-11-07 10:07:10 -0500

    setup

---
+++ models/userModel.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema({
+  name: { type: String },
+  description: { type: String },
+  email: { type: String },
+  phone: { type: String },
+  twitter: { type: String },
+  linkedin: { type: String },
+  github: { type: String },
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User
